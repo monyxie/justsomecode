b62a5fd14af97b374ad6dd354051ad4392f9264a
Mony Xie <monyxie@gmail.com>
2012-11-16 16:32:28 +0800

    indent some code

--- php/getset.php
+++ php/getset.php
@@ -1,17 +1,17 @@
 <?php
 class Singleton {
-private static $props = array();
-public function __construct() {}
-public function __get($name)
-{
-if(array_key_exists($name, self::$props)) {
-return self::$props[$name];
-}
-}
-public function __set($name, $value)
-{
-self::$props[$name] = $value;
-}
+    private static $props = array();
+    public function __construct() {}
+    public function __get($name)
+    {
+        if(array_key_exists($name, self::$props)) {
+            return self::$props[$name];
+        }
+    }
+    public function __set($name, $value)
+    {
+        self::$props[$name] = $value;
+    }
 }
 $a = new Singleton;
 $b = new Singleton;
