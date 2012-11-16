4d0897f83d7904dd2331880bd567ad463983aa7d
Mony Xie <monyxie@gmail.com>
2012-11-16 17:34:48 +0800

    modify some code

--- php/sendmail.php
+++ php/sendmail.php
@@ -5,8 +5,8 @@ $message = "Line 1\nLine 2\nLine 3";
 // In case any of our lines are larger than 70 characters, we should use wordwrap()
 $message = wordwrap($message, 70);
 
-ini_set('sendmail_from', 'monyxie@example.com');
+ini_set('sendmail_from', 'john@example.com');
 // Send
-mail('monyxie@example.com', 'My Subject', $message);
+mail('john@example.com', 'My Subject', $message);
 
 ?>
